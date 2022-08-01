966f3d37d80ac43ac9f7a9e19f5220170fb5fbf4
Sambit Kumar Nayak <77062786+sambit985@users.noreply.github.com>
2022-08-01 14:24:13 +0530

    Create replacePi.java

---
+++ Recursion/replacePi.java
@@ -0,0 +1,21 @@
+/*
+Wap in java to replace 3.14 in place of pi.
+*/
+
+public class Solution{
+    public static String replacepi(String str){
+        if(str.length()<=1){
+            return str;
+        }
+      if(str.charAt(0)=='p'&& str.charAt(1)=='i'){
+          String smallOutput=replacepi(str.substring(2));
+          return 3.14+smallOutput;
+      }else{
+          String smallOutput=replacepi(str.substring(1));
+          return str.charAt(0)+smallOutput;
+      }
+    }
+    public static void main(String[] args){
+        System.out.print(replacepi("apipivcipi"));
+    }
+}
