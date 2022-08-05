8fad111bb39ca52b72deb7efc2259bbe7ba00705
Sambit Kumar Nayak <77062786+sambit985@users.noreply.github.com>
2022-08-05 10:57:25 +0530

    Create ConvertStringToInt.java

---
+++ Recursion/ConvertStringToInt.java
@@ -0,0 +1,10 @@
+/*
+Wap to convert the string into corresponding integer and return the answer.
+
+*/
+public class Solution{
+  public static int convertStringToInt(String input){
+Integer i=Integer.valueOf(s); 
+return i;
+  }
+}
